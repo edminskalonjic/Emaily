62b1a46ff79b046a6149d8bfe22a3d15c6615194
Edmin Skalonjic <edminskalonjic@outlook.com>
2019-04-05 21:41:27 +0200

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports={
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    mongoUri : process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const CookieSession = require('cookie-session');
+require('./models/User');
+require('./services/passport');
 
 
 const app = express();
 
-app.get('', (req, res) =>{
-    res.send({hi : 'there'});
-
+mongoose.connect(keys.mongoUri, {useNewUrlParser:true}, (err)=> {
+    if(err){
+        console.log('error');
+    }
 });
+
+app.use (CookieSession({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
